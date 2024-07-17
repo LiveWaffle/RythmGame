7ed0f9944b9a0ac3621aba31fc4ce6f228238914
LiveWaffle <livewaffle@proton.me>
2024-07-17 14:13:35 -0400

    Move images to fix stupid error and background

--- Game.py
+++ Game.py
@@ -1,21 +1,22 @@
 import pygame
 
-pygame.init
-screen = pygame.display.set_mode((540,960))
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((540, 960))
 clock = pygame.time.Clock()
 running = True
 
-Background = pygame.image.load("")
-
+background = pygame.image.load("background.png")
 while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-  
+    screen.blit(background, (0, 0))
 
+    pygame.display.flip()
 
+    clock.tick(60)  # limits FPS to 60
 
-    clock.tick(60)
 pygame.quit()
