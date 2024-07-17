b522f3f13f76aab53043681a95161329c94dba80
LiveWaffle <livewaffle@proton.me>
2024-07-17 14:28:26 -0400

    Button Press!

--- Game.py
+++ Game.py
@@ -7,6 +7,7 @@ running = True
 
 background = pygame.image.load("background.png")
 Abutton = pygame.image.load("Abutton.png")
+Abuttonpressed = pygame.image.load("PressedA.png")
 
 screen_width, screen_height = screen.get_size()
 button_width, button_height = Abutton.get_size()
@@ -14,13 +15,18 @@ button_width, button_height = Abutton.get_size()
 button_x = (screen_width - button_width) / 2
 button_y = (screen_height - button_height) / 2
 
+button_image = Abutton
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    if event.type == pygame.MOUSEBUTTONDOWN:
+            button_image = Abuttonpressed
+    elif event.type == pygame.MOUSEBUTTONUP:
+        button_image = Abutton
 
     screen.blit(background, (0, 0))
-    screen.blit(Abutton, (button_x, button_y))
+    screen.blit(button_image, (button_x, button_y))
 
     pygame.display.flip()
 
